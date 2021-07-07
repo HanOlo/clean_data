2885ba9b699541027ee0057e25ee242cc46a622e
Hans Seller <67608613+HanOlo@users.noreply.github.com>
2021-07-06 23:23:02 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -52,15 +52,19 @@ part2_df <- combined_df[,comb_names_keep]
 
 
 
-
-
-
-
 ## PART 3 - Use descriptive activity names for activities in the data set
 #
 
 
 
+part3_df <- part2_df %>% 
+  mutate(Activity = recode_factor(Activity, 
+                                  `1` = "WALKING", 
+                                  `2` = "WALKING_UPSTAIRS",
+                                  `3` = "WALKING_DOWNSTAIRS",
+                                  `4` = "SITTING",
+                                  `5` = "STANDING",
+                                  `6` = "LAYING"))
 
 
 
@@ -68,10 +72,27 @@ part2_df <- combined_df[,comb_names_keep]
 ## Part 4 - Label the data set with descriptive variable names
 #
 
+new_col_names <- names(part3_df)
 
+new_names_keep <- new_names %>% 
+  str_replace_all("^t", "time_") %>% 
+  str_replace_all("^f", "freq_") %>% 
+  str_replace_all("Body", "body_") %>% 
+  str_replace_all("Gravity", "gravity_") %>% 
+  str_replace_all("Acc-|Acc", "accelerometer_") %>% 
+  str_replace_all("Gyro-|Gyro", "gyroscope_") %>% 
+  str_replace_all("\\(\\)-|\\(\\)", "_") %>% # replaces ()- and - with _ 
+  str_replace("_$", " ") %>% # removes special cases where _ is at end
+  str_to_lower() # makes all lower case
 
-
+part4_df <- part3_df # copies dataframe from part 2 to part3_df
+colnames(part4_df) <- new_names_keep # applies new names to colmn names
 
 
 
 ## Part 5 - Create a data set with the average for each variable for each activity and each subject
+#
+
+part5_df <- part4_df %>% 
+  group_by(activity, subject) %>% 
+  summarize(across(where(is.numeric), mean, .names = "mean_{.col}"))
